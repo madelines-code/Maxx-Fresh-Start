dab2b4d6635860be57e17fa7de698f7e2f408ae3
madelines-code <armstrong.madeline@gmail.com>
2025-09-30 12:57:06 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import React, { Component } from "react"
 import "./App.css"
 import Home from "./components/Home"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { loading: false, msg: null }
+//   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+//   handleClick = api => e => {
+//     e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+//     this.setState({ loading: true })
+//     fetch("/.netlify/functions/" + api)
+//       .then(response => response.json())
+//       .then(json => this.setState({ loading: false, msg: json.msg }))
+//   }
 
-  render() {
-    const { loading, msg } = this.state
+//   render() {
+//     const { loading, msg } = this.state
 
-    return (
-      <p>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+//     return (
+//       <p>
+//         <br />
+//         <span>{msg}</span>
+//       </p>
+//     )
+//   }
+// }
 
 class App extends Component {
   render() {
